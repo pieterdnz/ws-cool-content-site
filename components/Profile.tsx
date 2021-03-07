96bd1c18763251eab22cfd13d19be4ed70816a44
Pieter Bos <50238540+pieterdnz@users.noreply.github.com>
2021-03-07 17:53:44 +0100

    added name to profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,9 @@
 import { IProfile } from '../interfaces';
 
-export const Profile = ({ title, description, imageUrl }: IProfile) => (
-	<div className='md:w-1/3 p-8'>
-		<img className='w-1/3 mx-auto mb-4 rounded-full' src={`${imageUrl}`} alt='' />
-		<h3 className='text-xl mb-1 font-semibold font-heading'>title</h3>
+export const Profile = ({ name, title, description, imageUrl }: IProfile) => (
+	<div className='md:w-1/3'>
+		<img className='mx-auto mb-4 rounded-full' src={`${imageUrl}`} alt='' />
+		<h3 className='text-xl mb-1 font-semibold font-heading'>{name}</h3>
 		<span>{title}</span>
 		<p className='mt-4 text-gray-400 leading-relaxed'>{description}</p>
 	</div>
