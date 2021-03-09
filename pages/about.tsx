4ece5654815ef6c262d6f0ae42e6d1513f5bbc1e
Pieter Bos <50238540+pieterdnz@users.noreply.github.com>
2021-03-09 15:33:51 +0100

    added page interface and getStaticProps to about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout';
 
+//import { GraphQLClient, gql } from 'graphql-request';
+//import { IPage } from '../interfaces';
+
 const AboutPage = () => (
 	<Layout title='About'>
 		<p>This is about me</p>
 	</Layout>
 );
+
+//export async function getStaticProps() {}
 
 export default AboutPage;
